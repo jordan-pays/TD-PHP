eed4cfc09c0c4316e53b519f96fc6dceda2e9b87
paysj <paysj@K129-1.dep-info.iutmontp.univ-montp2.fr>
2020-09-11 16:45:28 +0200

    up

---
+++ TD1/testVoiture.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Liste des Voitures :</title>
+    </head>
+   
+    <body>
+        <?php
+        require_once
+        ?>
+    </body>
+</html> 
